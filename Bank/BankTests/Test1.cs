06c2b0f3bdaa5890497eb416a090c490b134ebe1
overpayyy <maksym.slipchyshyn@microsoft.wsei.edu.pl>
2025-03-31 18:21:21 +0200

    konto tests 1

---
+++ Bank/BankTests/Test1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Bank.Tests
+{
+    [TestClass]
+    public class KontoTests
+    {
+        [TestMethod]
+        public void Wplata_DodajeKwoteDoBalansu()
+        {
+            var konto = new Konto("Jan Kowalski", 100);
+            konto.Wplata(50);
+            Assert.AreEqual(150, konto.Balans);
+        }
+
+        [TestMethod]
+        public void Wyplata_OdejmowanieKwotyOdBalansu()
+        {
+            var konto = new Konto("Jan Kowalski", 100);
+            konto.Wyplata(50);
+            Assert.AreEqual(50, konto.Balans);
+        }
+    }
+}
