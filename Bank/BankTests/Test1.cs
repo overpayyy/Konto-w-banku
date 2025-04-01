feab3aafc24414521e3953b08550deafe6b8591a
overpayyy <maksym.slipchyshyn@microsoft.wsei.edu.pl>
2025-04-01 21:45:22 +0200

    konto test 2

--- Bank/BankTests/Test1.cs
+++ Bank/BankTests/Test1.cs
@@ -21,5 +21,64 @@ namespace Bank.Tests
             konto.Wyplata(50);
             Assert.AreEqual(50, konto.Balans);
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Wyplata_NiewystarczajaceSrodki_RzucaWyjatek()
+        {
+            var konto = new Konto("Jan Kowalski", 100);
+            konto.Wyplata(150);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Wplata_KontoZablokowane_RzucaWyjatek()
+        {
+            var konto = new Konto("Jan Kowalski", 100);
+            konto.BlokujKonto();
+            konto.Wplata(50);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Wyplata_KontoZablokowane_RzucaWyjatek()
+        {
+            var konto = new Konto("Jan Kowalski", 100);
+            konto.BlokujKonto();
+            konto.Wyplata(50);
+        }
+
+        [TestMethod]
+        public void BlokujKonto_UstawiaZablokowaneNaTrue()
+        {
+            var konto = new Konto("Jan Kowalski", 100);
+            konto.BlokujKonto();
+            Assert.IsTrue(konto.Zablokowane);
+        }
+
+        [TestMethod]
+        public void OdblokujKonto_UstawiaZablokowaneNaFalse()
+        {
+            var konto = new Konto("Jan Kowalski", 100);
+            konto.BlokujKonto();
+            konto.OdblokujKonto();
+            Assert.IsFalse(konto.Zablokowane);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Wplata_NegatywnaKwota_RzucaWyjatek()
+        {
+            var konto = new Konto("Jan Kowalski", 100);
+            konto.Wplata(-50);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Wyplata_NegatywnaKwota_RzucaWyjatek()
+        {
+            var konto = new Konto("Jan Kowalski", 100);
+            konto.Wyplata(-50);
+        }
     }
 }
