50f286e550be639c7d6767e2e7fe871d3c979fed
overpayyy <maksym.slipchyshyn@microsoft.wsei.edu.pl>
2025-04-04 17:15:32 +0200

    kontoplus and kontolimit tests

---
+++ Bank/BankTests/KontoLimitTests.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Bank.Tests
+{
+    [TestClass]
+    public class KontoLimitTests
+    {
+        [TestMethod]
+        public void Konstruktor_InicjalizujePoprawnie()
+        {
+            string klient = "Jan Kowalski";
+            decimal balansNaStart = 1000m;
+            decimal limitDebetowy = 500m;
+
+            KontoLimit kontoLimit = new KontoLimit(klient, balansNaStart, limitDebetowy);
+
+            Assert.AreEqual(balansNaStart, kontoLimit.Balans - limitDebetowy);
+            Assert.AreEqual(limitDebetowy, kontoLimit.JednorazowyLimitDebetowy);
+        }
+
+        [TestMethod]
+        public void ZmienLimitDebetowy_UstawiaNowyLimit()
+        {
+            KontoLimit kontoLimit = new KontoLimit("Jan Kowalski", 1000m, 500m);
+            decimal nowyLimit = 300m;
+
+            kontoLimit.ZmienLimitDebetowy(nowyLimit);
+
+            Assert.AreEqual(nowyLimit, kontoLimit.JednorazowyLimitDebetowy);
+        }
+
+        [TestMethod]
+        public void Wplata_ZwiekszaBalans()
+        {
+            KontoLimit kontoLimit = new KontoLimit("Jan Kowalski", 1000m, 500m);
+            decimal kwotaWplaty = 200m;
+
+            kontoLimit.Wplata(kwotaWplaty);
+
+            Assert.AreEqual(1200m, kontoLimit.Balans - kontoLimit.JednorazowyLimitDebetowy);
+        }
+
+        [TestMethod]
+        public void Wyplata_ZmniejszaBalans()
+        {
+            KontoLimit kontoLimit = new KontoLimit("Jan Kowalski", 1000m, 500m);
+            decimal kwotaWyplaty = 200m;
+
+            kontoLimit.Wyplata(kwotaWyplaty);
+
+            Assert.AreEqual(800m, kontoLimit.Balans - kontoLimit.JednorazowyLimitDebetowy);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Wyplata_PrzekraczaLimitDebetowy_ThrowsInvalidOperationException()
+        {
+            KontoLimit kontoLimit = new KontoLimit("Jan Kowalski", 1000m, 500m);
+            decimal kwotaWyplaty = 1600m;
+
+            kontoLimit.Wyplata(kwotaWyplaty);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Wyplata_ZablokowaneKonto_ThrowsInvalidOperationException()
+        {
+            KontoLimit kontoLimit = new KontoLimit("Jan Kowalski", 1000m, 500m);
+            kontoLimit.Wyplata(1500m);
+
+            kontoLimit.Wyplata(100m);
+        }
+
+        [TestMethod]
+        public void Wplata_OdblokowujeKonto()
+        {
+            KontoLimit kontoLimit = new KontoLimit("Jan Kowalski", 1000m, 500m);
+            kontoLimit.Wyplata(1500m);
+
+            kontoLimit.Wplata(600m); 
+
+            Assert.IsFalse(kontoLimit.Balans < 0);
+        }
+    }
+}
--- Bank/BankTests/KontoPlusTests.cs
+++ Bank/BankTests/KontoPlusTests.cs
@@ -1,1 +1,86 @@
-﻿
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Bank.Tests
+{
+    [TestClass]
+    public class KontoPlusTests
+    {
+        [TestMethod]
+        public void Konstruktor_InicjalizujePoprawnie()
+        {
+            string klient = "Jan Kowalski";
+            decimal balansNaStart = 1000m;
+            decimal limitDebetowy = 500m;
+
+            KontoPlus kontoPlus = new KontoPlus(klient, balansNaStart, limitDebetowy);
+
+            Assert.AreEqual(balansNaStart, kontoPlus.Balans - limitDebetowy);
+            Assert.AreEqual(limitDebetowy, kontoPlus.JednorazowyLimitDebetowy);
+        }
+
+        [TestMethod]
+        public void ZmienLimitDebetowy_UstawiaNowyLimit()
+        {
+            KontoPlus kontoPlus = new KontoPlus("Jan Kowalski", 1000m, 500m);
+            decimal nowyLimit = 300m;
+
+            kontoPlus.ZmienLimitDebetowy(nowyLimit);
+
+            Assert.AreEqual(nowyLimit, kontoPlus.JednorazowyLimitDebetowy);
+        }
+
+        [TestMethod]
+        public void Wplata_ZwiekszaBalans()
+        {
+            KontoPlus kontoPlus = new KontoPlus("Jan Kowalski", 1000m, 500m);
+            decimal kwotaWplaty = 200m;
+
+            kontoPlus.Wplata(kwotaWplaty);
+
+            Assert.AreEqual(1200m, kontoPlus.Balans - kontoPlus.JednorazowyLimitDebetowy);
+        }
+
+        [TestMethod]
+        public void Wyplata_ZmniejszaBalans()
+        {
+            KontoPlus kontoPlus = new KontoPlus("Jan Kowalski", 1000m, 500m);
+            decimal kwotaWyplaty = 200m;
+
+            kontoPlus.Wyplata(kwotaWyplaty);
+
+            Assert.AreEqual(800m, kontoPlus.Balans - kontoPlus.JednorazowyLimitDebetowy);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Wyplata_PrzekraczaLimitDebetowy_ThrowsInvalidOperationException()
+        {
+            KontoPlus kontoPlus = new KontoPlus("Jan Kowalski", 1000m, 500m);
+            decimal kwotaWyplaty = 1600m;
+
+            kontoPlus.Wyplata(kwotaWyplaty);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Wyplata_ZablokowaneKonto_ThrowsInvalidOperationException()
+        {
+            KontoPlus kontoPlus = new KontoPlus("Jan Kowalski", 1000m, 500m);
+            kontoPlus.Wyplata(1500m); 
+
+            kontoPlus.Wyplata(100m);
+        }
+
+        [TestMethod]
+        public void Wplata_OdblokowujeKonto()
+        {
+            KontoPlus kontoPlus = new KontoPlus("Jan Kowalski", 1000m, 500m);
+            kontoPlus.Wyplata(1500m); 
+
+            kontoPlus.Wplata(600m); 
+
+            Assert.IsFalse(kontoPlus.Balans < 0);
+        }
+    }
+}
